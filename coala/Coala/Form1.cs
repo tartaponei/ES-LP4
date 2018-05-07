cd82c7601982d5d5d5a3d8ab049c4c891e0f8691
Lucas Fernandes <lucasfernandes.um@gmail.com>
2018-05-07 14:51:00 -0300

    trabalho do coala

---
+++ coala/Coala/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Coala
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAlteraImagem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+
+            dlg.Title = "Abrir imagem";
+            dlg.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.ImageLocation = dlg.FileName;
+            }
+
+            dlg.Dispose();
+        }
+    }
+}
