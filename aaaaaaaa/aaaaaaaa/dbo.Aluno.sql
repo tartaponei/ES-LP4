0f4147c7fede3aec8c1a14d4b7a514e6e4c86740
Sarah <sarahborretti@gmail.com>
2018-08-20 15:15:17 -0300

    Projeto com Banco de Dados

---
+++ aaaaaaaa/aaaaaaaa/dbo.Aluno.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Aluno]
+(
+	[Matricula] INT NOT NULL PRIMARY KEY, 
+    [Nome] NCHAR(50) NULL, 
+    [DataNascimento] DATETIME NULL, 
+    [Sexo] NCHAR(1) NULL
+)
---
+++ aaaaaaaa/aaaaaaaa/dbo.Disciplina.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Disciplina]
+(
+	[IdDisciplina] INT NOT NULL PRIMARY KEY, 
+    [Nome] NCHAR(50) NULL, 
+    [IdProfessor] NCHAR(50) NULL
+)
