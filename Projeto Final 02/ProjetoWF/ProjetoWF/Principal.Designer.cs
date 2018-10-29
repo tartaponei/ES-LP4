de77840672d81643143b2844097facf2a37a41c3
Sarah <sarahborretti@gmail.com>
2018-10-29 12:45:27 -0300

    Projeto Final

---
+++ Projeto Final 02/ProjetoWF/ProjetoWF/Principal.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ProjetoWF
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Trebuchet MS", 30F);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 49);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Bem-vindo(a)!";
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProjetoWF.Properties.Resources.background_projeto;
+            this.ClientSize = new System.Drawing.Size(935, 475);
+            this.Controls.Add(this.label2);
+            this.Name = "Principal";
+            this.Text = "Bem-vindo(a)!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Projeto Final/aaaaaaaa/aaaaaaaa/dbo.Professor.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Professor]
+(
+	[Matricula] INT NOT NULL PRIMARY KEY, 
+    [Nome] NCHAR(50) NULL, 
+    [DtNascimento] DATETIME NULL, 
+    [Sexo] NCHAR(1) NULL, 
+    [Formação] NCHAR(50) NULL
+)
